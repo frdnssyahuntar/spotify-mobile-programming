e0aae19783afb957aaa47c12fcaafb0aa5ff6201
ramdant29 <ramdanthalib3@gmail.com>
2026-09-06 14:33:13 +0700

    menambahkan bottom navigation bar

---
+++ lib/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff121212),
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: const Color(0xff121212),
+        foregroundColor: const Color(0xffffffff),
+        elevation: 0
+      ),
+      body: const Center(
+        child: Text('tes'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xff121212),
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xffffffff),
+        unselectedItemColor: const Color(0xff757575),
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: _navBarItems,
+      ),
+    );
+  }
+}
+
+const _navBarItems = [
+  BottomNavigationBarItem(
+    icon: Icon(Icons.home_outlined),
+    activeIcon: Icon(Icons.home_rounded),
+    label: 'Home',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.search),
+    activeIcon: Icon(Icons.search),
+    label: 'Search',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.library_music_outlined),
+    activeIcon: Icon(Icons.library_music),
+    label: 'Koleksi Kamu',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.add_outlined),
+    activeIcon: Icon(Icons.add),
+    label: 'Buat',
+  ),
+];
